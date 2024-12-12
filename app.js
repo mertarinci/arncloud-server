401e72d0b1f963c2eec5ce59b488de1d6e142ab1
mertarinci <mertarinci96@gmail.com>
2024-12-12 15:16:35 +0100

    error message added, version update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/api/files', fileRoutes);
 app.use('/api/users', userRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Cloud Storage API v.0.3.0");
+    res.send("Welcome to Cloud Storage API v.0.3.1");
 })
 
 
--- controllers/auth.js
+++ controllers/auth.js
@@ -95,7 +95,7 @@ const forgotPassword = async (req, res) => {
         res.status(200).json({ message: "Password reset email sent." });
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: "Failed to process request." });
+        res.status(500).json({ message: "Failed to process request.", err: err });
     }
 };
 
